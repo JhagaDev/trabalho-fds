c7da7a7badfc4e8acca4d274c9a60492cf3c1afd
JhagaDev <jhagadev@gmail.com>
2025-06-27 20:15:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+    function mostrarmensagem() {
+            const mensagem = document.getElementById("msg");
+            mensagem.textContent = "O conhecimento é a chave para mudar o mundo. Estude e faça parte da transformação!";
+            mensagem.style.display = "block";
+        }
